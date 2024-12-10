10e1beb7797a2c41f61e7ed68b89e7f5f5d104e2
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-12-10 14:48:02 -0300

    Configure response for add custom header on response (#35)

---
+++ src/EventSubscriber/CustomHttpResponseSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CustomHttpResponseSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        $event
+            ->getResponse()
+            ->headers
+            ->set('X-REQUEST-INFO', '2');
+    }
+}
